8efab90598a4ac7ce9c2ce8ebbc02cf35cac4907
corgimaman <78245727+corgimaman@users.noreply.github.com>
2021-06-06 21:42:58 -0500

    added partials and date util

---
+++ utils/helpers.js
@@ -0,0 +1,15 @@
+module.exports = {
+    format_time: (date) => {
+      return date.toLocaleTimeString();
+    },
+    format_date: (time) => {
+      let yyyy = time.getFullYear();
+      let d = time.getDate();
+      let m = time.getMonth();
+  
+      let formattedDate = m + '/' + d + '/' + yyyy;
+  
+      return formattedDate;
+    }
+  };
+  
