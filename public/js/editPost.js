3ee8063bed121c837a66efd8c58d3f45f9eb90c7
corgimaman <78245727+corgimaman@users.noreply.github.com>
2021-06-07 23:39:45 -0500

    ready to test

---
+++ public/js/editPost.js
@@ -0,0 +1,25 @@
+const editPostHandler = async (event) => {
+    event.preventDefault();
+
+    const title = document.querySelector('#post_title').value.trim();
+    const body = document.querySelector('#post_body').value.trim();
+    const post_id = document.querySelector('#post_id').value.trim();
+
+    if (title && body) {
+        const response = await fetch(`/api/posts/${post_id}`, {
+            method: 'PUT',
+            body: JSON.stringify({ title, body }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        if(response.ok) {
+            alert("Post updated successfully!");
+            document.location.replace('/dashboard');
+        } else {
+            alert(response.statusText);
+        }
+    }
+};
+
+
+document.querySelector('#editPost').addEventListener('submit', editPostHandler);
